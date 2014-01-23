0adf6f097c789b004a4acf6bed3abb9f51cb0e8e
Charles Jessup Franklin <cjfman@gmail.com>
2014-01-23 16:06:31 -0500

    Added argument parsing and opening binary files

--- src/megasim.c
+++ src/megasim.c
@@ -12,41 +12,125 @@
 #include <stdlib.h>
 #include <string.h>
 #include <inttypes.h>
+#include <unistd.h>
+#include <ctype.h>
 #include "megasim.h"
 
 int main(int argc, char* argv[]) {
-	if (argc != 2) {
-		print_usage();
+	int error = 0;
+	// Parse arguments
+	error = parseArgs(argc, argv, &args);
+	if (error) return 1;
+	Program *program = loadData(&args);
+	if (!program) {
 		return 1;
-	}
-	// Open file
-	char* file_path = argv[1];
-	FILE* prog_file = fopen(file_path, "rb");
-	if (!prog_file) {
-		fprintf(stderr, "Error: file '%s' does not exist\n", file_path);
-		return 1;
-	}
-	// Get file size
-	fseek(prog_file, 0, SEEK_END);
-	int prog_size = ftell(prog_file)/sizeof(uint16_t);
-	fseek(prog_file, 0, SEEK_SET);
-	// Read entire file into buffer
-	uint16_t* program = (uint16_t*)malloc(prog_size*sizeof(uint16_t));
-	fread(program, sizeof(uint16_t), prog_size, prog_file);
-	fclose(prog_file);
-	// Print file Test
-	int i;
-	for (i = 0; i < prog_size; i++) {
-		printf("%2x", program[i]);
-		if (i%16 == 0) printf("\n");
 	}
 	return 0;
 }
 
-void print_usage(void) {
-	fprintf(stderr, "Usage: megasim [file name]\n");
+///////////////////////////////////////////
+// File Parsing
+///////////////////////////////////////////
+
+Program* loadData(Args* args) {
+	// Determin type of file and opening mode
+	char* path = args->path;
+	bool isHex = (strcmp("hex", getFileExt(path)) == 0);
+	if (args->hex || isHex) {
+		return loadHexFile(path);
+	}
+	else {
+		return loadBinFile(path);
+	}
 }
 
-void print_error(const char* message){
+Program* loadBinFile(const char* path) {
+	Program *program = (Program*)malloc(sizeof(Program));
+	// Open file
+	FILE* file = fopen(path, "r");
+	if (!file) {
+		fprintf(stderr, "Error: file '%s' does not exist\n", path);
+		return NULL;
+	}
+	int size = getFileSize(file);
+	program->size = size/2;
+	// Allocate buffer memory
+	program->data = (uint16_t*)malloc(size);
+	// Read entire file into buffer
+	fread(program->data, sizeof(char), size, file);
+	fclose(file);
+	return program;
+}
+
+Program* loadHexFile(const char* path) {
+	return NULL;
+}
+
+const char* getFileExt(const char *filename) {
+	const char *dot = strchr(filename, '.');
+	if(!dot || dot == filename) return "";
+	return dot + 1;
+}
+
+int getFileSize(FILE* file) {
+	int pos = ftell(file);
+	fseek(file, 0, SEEK_END);
+	int size = ftell(file);
+	fseek(file, pos, SEEK_SET);
+	return size;
+}
+
+//////////////////////////////////////////////////
+// Arg parsing
+//////////////////////////////////////////////////
+
+void initArgs(Args* args) {
+        args->hex = false;
+	args->path = NULL;
+}
+
+int parseArgs(int argc, char* argv[], Args* args) {
+	initArgs(args);
+	int c;
+	while ((c = getopt(argc, argv, "h")) != -1) {
+		switch (c) {
+		case 'h':
+			args->hex = true;
+			break;
+		case '?':
+			if (isprint(optopt)) 
+				fprintf(stderr, "Unknown option '-%c'.\n", optopt);
+			else
+				fprintf(stderr, "Unknown unprintable option\n");
+			print_usage();
+			return 1;
+		default:
+			fprintf(stderr, "Unknown error while parsing arguments\n");
+			return 1;
+
+		}
+	}
+	if (argc - optind < 1) {
+		print_usage();
+		return 1;
+	}
+	else if (argc - optind > 1) {
+		fprintf(stderr, "Too many non-option arguments\n");
+		print_usage();
+		return 1;
+	}
+	args->path = argv[optind];
+	return 0;	
+}
+
+//////////////////////////////////////////////////
+// Printing
+//////////////////////////////////////////////////
+
+void print_usage(void) {
+	fprintf(stderr, "Usage: megasim [options] [file name]\n");
+}
+
+void print_error(const char* message) {
 	fprintf(stderr, "Error: %s\n", message);
 }
--- src/megasim.h
+++ src/megasim.h
@@ -3,4 +3,60 @@
 //
 // MIT Lisense
 
+#include <stdio.h>
+#include <inttypes.h>
+
+// Boolean defines
+#define TRUE 1
+#define True 1
+#define true 1
+#define FALSE 0
+#define False 0
+#define false 0
+typedef uint8_t bool;
+
+//////////////////
+// Option Parsing
+//////////////////
+
+// A struct to hold options and arguments
+typedef struct Args {
+	bool hex; 	// Use hex file format
+	char* path;	// The path to the file
+} Args;
+
+Args args;	// Global arguments struct
+
+// Initializes an Args struct to default values
+void initArgs(Args* args);
+// Parses the arguments to the program
+int parseArgs(int argc, char* argv[], Args* args);
+
+
+///////////////////
+// Data Structures
+///////////////////
+
+typedef struct Program {
+	uint16_t *data;
+	int size;
+} Program;
+
+
+//////////////////
+// File Parsing
+//////////////////
+
+// Loads the file given in the arguments
+// and allocates memory for the entire file.
+// Binary files for AVR micros aren't that large.
+// Returns the pointer to the data.
+Program* loadData(Args* args);
+Program* loadHexFile(const char* path);
+Program* loadBinFile(const char* path);
+const char* getFileExt(const char *filename);
+int getFileSize(FILE* file);
+
+// Printing
 void print_usage(void);
+void print_error(const char* message);
